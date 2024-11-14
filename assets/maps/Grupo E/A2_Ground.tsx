2cfb0daa037eabd6ed3dc86d63fb0bdfc632e1b7
unknown <henrique.gau@hotmail.com>
2024-11-14 13:34:33 -0300

    mapa do grupo E

---
+++ assets/maps/Grupo E/A2_Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="A2_Ground" tilewidth="8" tileheight="8" tilecount="3072" columns="64">
+ <image source="../Downloads/tilesets/A2_Ground.png" width="512" height="384"/>
+</tileset>
---
+++ assets/maps/Grupo E/C_OutSide_Nature.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="C_OutSide_Nature" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
+ <image source="../Downloads/tilesets/C_OutSide_Nature.png" width="512" height="512"/>
+</tileset>
