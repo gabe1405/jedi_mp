b4baccdb031da5d7706b834b93c423c17cd84e40
Thomaz <thomazmbonfim@gmail.com>
2024-11-20 19:06:46 -0300

    adicionei os arquivos do grp g

---
+++ assets/maps/Grupo G/CampoMinadoSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="CampoMinado" tilewidth="32" tileheight="32" tilecount="160" columns="20">
+ <image source="Tiny Swords/Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Flat.png" width="640" height="256"/>
+</tileset>
