7cdc008410d82af2c79aefd4f4c033fac0426ffb
Eduardo Yuji <65960933+eduardo-yuji@users.noreply.github.com>
2025-02-17 21:36:52 -0300

    Add files via upload

---
+++ assets/maps/Grupo_C/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="Tileset" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="../../../../Mapa/Mapa/Tile_Asset_Png.png" width="1024" height="768"/>
+</tileset>
